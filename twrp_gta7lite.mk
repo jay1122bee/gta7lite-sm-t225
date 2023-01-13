44dde7123aceb0b2bc31d48223ed4cf5700f4979
jay1122bee <122219335+jay1122bee@users.noreply.github.com>
2023-01-13 22:39:48 +0000

    Add files via upload

--- twrp_gta7lite.mk
+++ twrp_gta7lite.mk
@@ -1,1 +1,42 @@
-## Copyright (C) 2023 The Android Open Source Project# Copyright (C) 2023 SebaUbuntu's TWRP device tree generator## SPDX-License-Identifier: Apache-2.0# # Inherit from those products. Most specific first.$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk) # Inherit some common Omni stuff.$(call inherit-product, vendor/twrp/config/common.mk) # Inherit from gta7lite device$(call inherit-product, device/samsung/gta7lite/device.mk) PRODUCT_DEVICE := gta7litePRODUCT_NAME := twrp_gta7litePRODUCT_BRAND := samsungPRODUCT_MODEL := SM-T225PRODUCT_MANUFACTURER := samsung PRODUCT_GMS_CLIENTID_BASE := android-samsung-ss PRODUCT_BUILD_PROP_OVERRIDES += \ PRIVATE_BUILD_DESC="gta7litexx-user 11 RP1A.200720.012 T225XXU1AVE1 release-keys" BUILD_FINGERPRINT := samsung/gta7litexx/gta7lite:11/RP1A.200720.012/T225XXU1AVE1:user/release-keys
+#
+# Copyright (C) 2020 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Release name
+PRODUCT_RELEASE_NAME := gta7lite
+
+# Inherit from common AOSP config
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
+
+# Inherit some common TWRP stuff.
+# For PitchBlack use vendor/pb/config/common.mk
+$(call inherit-product, vendor/twrp/config/common.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/samsung/gta7lite/device.mk)
+
+# Charger
+PRODUCT_PACKAGES += \
+    charger_res_images
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,device/samsung/gta7lite/recovery/root,recovery/root)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_NAME := twrp_gta7lite
+PRODUCT_DEVICE := gta7lite
+PRODUCT_MODEL := SM-T225
+PRODUCT_BRAND := samsung
+PRODUCT_MANUFACTURER := samsung
+PRODUCT_GMS_CLIENTID_BASE := android-samsung
